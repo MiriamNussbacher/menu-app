480708be89e2347ffc826ceb5aa9cb4f5941e56f
Miriam Nussbacher <miriamnussbacher@gmail.com>
2024-07-24 03:41:25 +0300

    Cache react query, fix ui and add read.me file

--- README.md
+++ README.md
@@ -6,3 +6,35 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# Family Hierarchy Application
+
+Thanks for reading my React assignment!
+
+This application displays all members from the server. Once a specific member is selected, the full hierarchy is displayed up to the root member.
+
+## Technologies
+
+- **Vite**: The project was created using Vite, a  tool for modern web projects.
+- **Material-UI**: Components are build and designed using Material-UI.
+- **React Query**: Data fetching and caching are managed with React Query, which caches data for a configured amount of milliseconds.
+- **Axios**: HTTP client for making requests to the members API.
+
+## Features
+
+- **Display Members**: Fetches and displays a list of all members from the server.
+- **View Hierarchy**: Displays the full hierarchy of a selected member, up to the root member.
+- **Caching**: Data is cached using React Query to minimize redundant network requests.
+
+
+### Steps
+
+1. **Clone the repository**:
+
+   git clone https://github.com/miriamnussbacher/menu-app.git
+  
+
+
+
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,88 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { MenuList, MenuItem, ListItemText } from '@mui/material';
+import { AppBar, Toolbar, Typography, Container, MenuList, MenuItem, ListItemText, Paper } from '@mui/material';
 import { useQuery, QueryClient, QueryClientProvider } from 'react-query';
 import axios from 'axios';
 
+const queryClient = new QueryClient();
+
+
+const fetchHierarchy = async (memberId) => {
+  const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/members/${memberId}`);
+  return response.data;
+};
+
+const fetchRoot = async () => {
+  const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/members`);
+  return response.data;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const queryClient = new QueryClient();
-
-  
-  const fetchHierarchy = async (memberId) => {
-    const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/members/${memberId}`);
-    return response.data;
-  };
-
-  const fetchRoot = async() =>{
-    const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/members`);
-    return response.data;
-  }
+  const [selectedMemberId, setSelectedMemberId] = useState(null);
 
   const handleMenuItemClick = (memberId) => {
     setSelectedMemberId(memberId);
   };
 
-  const { data: parents } = useQuery('parents', fetchRoot);
+  const { data: parents } = useQuery('parents', fetchRoot,{
+    staleTime: import.meta.env.VITE_QUERY_CACHE_TIME, 
+    cacheTime: import.meta.env.VITE_QUERY_CACHE_TIME, 
+    refetchOnWindowFocus: false,
+  });
 
-//old working code!!!
   const renderMenuItems = (parents) => {
+    debugger
     if (!parents) return null;
     return parents.map((item) => {
-      if (item.children) {
-        return (
-          <MenuItem key={item.memberId}>
-            <ListItemText primary={item.name} />
-            <MenuList className='MuiMenuItem-root'>
-              {renderMenuItems(item.children)}
-            </MenuList>
-          </MenuItem>
-        );
-      }
-
-   
-
       return (
-        <MenuItem key={item.memberId}>
+        <MenuItem key={item.memberId} onClick={() => handleMenuItemClick(item.memberId)}
+          className={selectedMemberId === item.memberId ? 'selected' : ''}>
           <ListItemText primary={item.name} />
+          <MenuList className='MuiMenuItem-root'>
+            {renderMenuItems(item.children)}
+          </MenuList>
         </MenuItem>
       );
+
     });
   };
 
   return (
-    <div>
-    <MenuList>
-      {renderMenuItems(parents)}
-    </MenuList>
-  </div>
+    
+    <div className="container">
+       <AppBar position="fixed">
+         
+            <Typography variant="h6">
+              Members Hierarchy
+            </Typography>
+    
+        </AppBar>
+      <MenuList className="menu">
+        {renderMenuItems(parents)}
+      </MenuList>
+      {selectedMemberId && <Hierarchy memberId={selectedMemberId} />}
+    </div>
   )
+}
+
+function Hierarchy({ memberId }) {
+  const { data: hierarchy } = useQuery(['hierarchy', memberId], () => fetchHierarchy(memberId), {
+    enabled: !!memberId, 
+    staleTime: import.meta.env.VITE_QUERY_CACHE_TIME, 
+    cacheTime: import.meta.env.VITE_QUERY_CACHE_TIME, 
+    refetchOnWindowFocus: false,
+  });
+  
+  if (!hierarchy) return null;
+  return (
+    <MenuList className="hierarchy">
+      {hierarchy.map((member) => (
+        <MenuItem key={member.memberId}>
+          <ListItemText primary={member.name} />
+        </MenuItem>
+      ))}
+    </MenuList>
+  );
 }
 
 export default App
